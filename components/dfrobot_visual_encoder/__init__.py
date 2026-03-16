d8d111a8a2f95329e9d967669deca533f0d0aa70
atsukirasan-web <atsukirasan@gmail.com>
2026-03-16 14:37:39 +0000

    Update __init__.py

--- components/dfrobot_visual_encoder/__init__.py
+++ components/dfrobot_visual_encoder/__init__.py
@@ -1,9 +1,13 @@
 import esphome.config_validation as cv
-from esphome.const import CONF_ID, CONF_ON_PRESS, CONF_ON_LONG_PRESS, CONF_ON_CLOCKWISE, CONF_ON_COUNTER_CLOCKWISE
+from esphome.const import CONF_ID
+from esphome.cpp_types import Component, i2c
 from esphome.cpp_generator import Pvariable, add
-from esphome.core import Lambda
-from esphome.components import sensor, binary_sensor
-from esphome.cpp_types import Component, gpio, i2c
+
+# Define the automation keys manually
+CONF_ON_PRESS = "on_press"
+CONF_ON_LONG_PRESS = "on_long_press"
+CONF_ON_CLOCKWISE = "on_clockwise"
+CONF_ON_COUNTER_CLOCKWISE = "on_counter_clockwise"
 
 DEPENDENCIES = ["i2c"]
 
@@ -24,18 +28,14 @@ def to_code(config):
     add(var)
 
     if CONF_ON_CLOCKWISE in config:
-        automation = config[CONF_ON_CLOCKWISE]
-        for call in automation:
+        for call in config[CONF_ON_CLOCKWISE]:
             var.add_on_clockwise_callback(call)
     if CONF_ON_COUNTER_CLOCKWISE in config:
-        automation = config[CONF_ON_COUNTER_CLOCKWISE]
-        for call in automation:
+        for call in config[CONF_ON_COUNTER_CLOCKWISE]:
             var.add_on_counter_clockwise_callback(call)
     if CONF_ON_PRESS in config:
-        automation = config[CONF_ON_PRESS]
-        for call in automation:
+        for call in config[CONF_ON_PRESS]:
             var.add_on_press_callback(call)
     if CONF_ON_LONG_PRESS in config:
-        automation = config[CONF_ON_LONG_PRESS]
-        for call in automation:
+        for call in config[CONF_ON_LONG_PRESS]:
             var.add_on_long_press_callback(call)
